0f42b296dcec4171e58b4f471144c4df2adcb90c
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2022-02-24 19:46:35 -0300

    delete function add to delete data from db

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import database
 
-database.add_one("Peter","Parker","peterparker@spiderman.com")
+#database.add_one("Peter","Parker","peterparker@spiderman.com")
+
+#Delete record use a rowid as a STRING
+database.delete_one('6')
 database.show_all()
 
 
--- database.py
+++ database.py
@@ -6,11 +6,9 @@ def show_all():
     conn = sqlite3.connect("customer.db")
     #Create a cursor
     c = conn.cursor()
-
     #Query the database
     c.execute("SELECT rowid, * FROM customers")
     items = c.fetchall()
-
     #Looping and showing all the records.
     for item in items:
         print(item)
@@ -24,8 +22,15 @@ def show_all():
 def add_one(first,last,email):
     conn = sqlite3.connect("customer.db")
     c = conn.cursor()
-
     c.execute("INSERT INTO customers VALUES(?,?,?)", (first,last,email))
-
     conn.commit()
     conn.close()
+
+#Delete a record from table
+def delete_one(id):
+    conn = sqlite3.connect("customer.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM customers WHERE rowid = ?", id)
+    conn.commit()
+    conn.close()
+
